9ef9e6f4f2b8730292dff5e144ec7cac3cf6d88a
Iurii Oliiar <ioliiar@macmini40.ddns.softservecom.com>
2013-09-13 19:01:44 +0300

    Initial Commit

---
+++ ExternalBonjourScreen/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ExternalBonjourScreen
+//
+//  Created by Iurii Oliiar on 9/13/13.
+//  Copyright (c) 2013 Iurii Oliiar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
